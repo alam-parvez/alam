1c9b2d999af97b3893f819e8ce752b0502bc50e6
Parvez Alam <61111889+alam-parvez@users.noreply.github.com>
2024-10-04 16:14:59 +0000

    datatypes

---
+++ 02_alam/02_datatypes.js
@@ -0,0 +1,11 @@
+// Program to print the swap of numbers without using third variable in built in functions
+
+var a = 10
+var b = 5
+
+a = a + b
+b = a - b
+a = a - b
+
+console.log(a)
+console.log(b)
